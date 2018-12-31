74c531979d641b4dd5823c8e8fc807997a6fffd0
Jean-Claude Parent <noursicain@gmail.com>
2018-12-31 00:53:13 -0500

    Commit initial

---
+++ DocDocosTestNuget/TestMacbetcire21.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocDocosTestNuget
+{
+    [TestClass]
+    public class TestMacbetcide21
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
