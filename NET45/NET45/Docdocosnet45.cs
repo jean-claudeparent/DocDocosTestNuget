998bff7376ab66bf52d6f5010d0973eaed5b3a91
Jean-Claude Parent <noursicain@gmail.com>
2019-01-01 11:36:20 -0500

    Création du test net 45

---
+++ NET45/NET45/Docdocosnet45.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocDocos;
+
+
+namespace NET45
+{
+    [TestClass]
+    public class Docdocosnet45
+    {
+        [TestMethod]
+        public void GenererOK()
+        {
+            DocDocosDA generateur = new DocDocosDA();
+
+        }
+    }
+}
---
+++ NET45/NET45/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NET45")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NET45")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02f5aa98-ba0b-4573-bf64-5deb8d8d7292")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
